db21faa947efcd40143a49e249be827f1f5f90ca
sreekanthzzzz <bollepalli.sreekanth@incedoinc.com>
2022-12-08 10:18:38 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<div>
+    <h1><em>I'm Sreekanth</em></h1>
+    <h2>a Programmer.</h2>
+    <h2>Technical Skills:</h2>
+    <li>
+    python
+    </li>
+    <li>c++</li>
+    <li>Java</li>
+    <li>Javascript</li>
+    <h2>Conatct info</h2>
+    <p>Mobile Number:7358665761</p>
+    <p>bsreekanth814@gmail.com</p>
+    
+
+  </div>
+
+  );
+}
+
+export default App;
